090847d328ee283f2e19143fb5e2abe076d90f45
Chris Wolf <chriswolf@Chriss-MacBook-Pro.local>
2017-08-20 18:41:10 -0400

    Completed Question1

---
+++ master/Lecture1/Question1/Question1.rkt
@@ -0,0 +1,5 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Question1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;If a right triangle has the sides of a = 3 and b = 4 what is c?
+(sqrt (+ (sqr 3) (sqr 4)))
